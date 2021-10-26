b33679275f8483ca68ce326860ff208203e2bda3
Simeon Ayara <simeondigital@gmail.com>
2021-10-26 09:19:05 +0100

    Using C# to impliment quick sort

---
+++ Quick_Sort/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Quick_Sort
+{
+    class Program
+    {
+        private static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(arr, left, right);
+
+                if (pivot > 1)
+                {
+                    QuickSort(arr, left, pivot - 1);
+                }
+                if (pivot + 1 < right)
+                {
+                    QuickSort(arr, pivot + 1, right);
+                }
+            }
+
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int pivot = arr[left];
+            while (true)
+            {
+
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right]) return right;
+
+                    int temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+
+
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+
+        public static void PrintSorted(int[] arr)
+        {
+           // int[] sorted = ;
+
+            Console.WriteLine("Original unsorted array : ");
+            int[] unsorted = arr;
+            for (int i = 0; i < unsorted.Length; i++)
+            {
+                Console.Write(unsorted[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            QuickSort(arr, 0, arr.Length - 1);
+
+            Console.WriteLine();
+            Console.WriteLine("Sorted array : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" " + arr[i]);
+            }
+
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 6, 21, 87, 14 };
+            PrintSorted(arr);           
+        }     
+    }
+}
